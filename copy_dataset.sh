49c77b18a3447eb33e8a9f1cd2510599b7667969
Alan <alan.tellecom@gmail.com>
2021-06-22 15:45:44 -0300

    init

---
+++ copy_dataset.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+rm -rf imagens/doenca_fungica_videira/dataset/*
+cp -r imagens/doenca_fungica_videira/positivo/* imagens/doenca_fungica_videira/dataset
+cp -r imagens/doenca_fungica_videira/negativo/* imagens/doenca_fungica_videira/dataset
